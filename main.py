e1e0c1226c5bf9836b4766ba937eb350a2a44a8a
MinHius <125289254+MinHius@users.noreply.github.com>
2024-05-08 21:56:08 +0700

    check loss, accuracy

--- main.py
+++ main.py
@@ -7,19 +7,31 @@ import tensorflow as tf
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = tf.keras.utils.normalize(x_train, axis = 1)
-x_test = tf.keras.utils.normalize(x_test, axis = 1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape = (28, 28)))
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(128, activation = 'relu'))
-model.add(tf.keras.layers.Dense(10, activation = 'softmax'))
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs = 3)
+model.fit(x_train, y_train, epochs=3)
 
-model.save('handwritten.keras')
+# Saving the model
+model.save('handwritten.h5')
+
+# Loading the model
+loaded_model = tf.keras.models.load_model('handwritten.h5')
+
+loss, accuracy = loaded_model.evaluate(x_test, y_test)
+print("Loss:", loss)
+print("Accuracy:", accuracy)
+
 
 

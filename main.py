0d261cdeb7ca76615d046363490956abed3b78fa
MinHius <125289254+MinHius@users.noreply.github.com>
2024-05-15 22:38:54 +0700

    organizing src

--- main.py
+++ main.py
@@ -2,49 +2,9 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-# Lấy bộ dữ liệu mnist và cache.
-def get_mnist_data():
-    
-    # Lấy bộ dữ liệu mnist. 
-    path = 'mnist.npz'
-
-    # Cache.
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data(path=path)
-    return x_train, y_train, x_test, y_test
-
-# Huấn luyện mô hình với bộ dữ liệu mnist. 
-def train_model(x_train, y_train, x_test, y_test):
-    
-    # Lặp cho tới khi độ chính xác > 99%.
-    class myCallback(tf.keras.callbacks.Callback):
-        def on_epoch_end(self, epoch, logs={}):
-            if logs.get('accuracy') > 0.99:
-                print("\nReached 99% accuracy, stopping training!")
-                self.model.stop_training = True    
-    callbacks = myCallback()
-
-    # Chuẩn hóa dữ liệu.
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    # Tạo mô hình CNN với lớp Flatten và 2 lớp Dense.
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
-        tf.keras.layers.Dense(512, activation=tf.nn.relu),
-        tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-    ])
-
-    # Hàm tối ưu ADAM, hàm mất mát sparse categorical crossentropy, đánh giá mô hình bằng accuracy.
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    print(model.summary())
-
-    # Fit mô hình.
-    history = model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
- 
-    print(history.epoch, history.history['accuracy'][-1])
-    return model
+# Define custom softmax_v2 function
+def softmax_v2(x):
+    return tf.nn.softmax(x)
 
 # Dự đoán chữ số trong ảnh thu được.
 def predict(model, img):
@@ -112,32 +72,11 @@ def start_cv(model):
     cap.release()
     cv2.destroyAllWindows()
 
-# Hàm main.
-def main():
-    model = None
-    # Nếu đã có model, không cần phải train nữa mà chạy model luôn.
-    try:
-        model = tf.keras.models.load_model('model.h5')
-        print('Loaded saved model.')
-        print(model.summary())
-    # Không có model thì sẽ huấn luyện model mới.
-    except:
-        print("Getting mnist data...")
-        (x_train, y_train, x_test, y_test) = get_mnist_data()
-        print("Training model...")
-        model = train_model(x_train, y_train, x_test, y_test)
-        print("Saving model...")
-        model.save('model.h5')
-    
-    print("Starting cv...")
+def run():
+    # Load the model with custom objects
+    model = tf.keras.models.load_model('model.h5', custom_objects={'softmax_v2': softmax_v2})
+    print('Loaded saved model.')
+    print(model.summary())  
     start_cv(model)
-
-# Khởi chạy.
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
+    
+run()

a2c928a75d385def788ba5f8f08324641e499cda
MinHius <125289254+MinHius@users.noreply.github.com>
2024-05-21 01:09:47 +0700

    fix

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
-# Define custom softmax_v2 function
+# Hàm softmax biến 10 đầu ra từ mạng nơ-ron thành xác suất.
 def softmax_v2(x):
     return tf.nn.softmax(x)
 
-# Dự đoán chữ số trong ảnh thu được.
+# Dự đoán chữ số trong ảnh thu được bằng hàm argmax - lấy xác suất cao nhất trong 10 xác suất.
 def predict(model, img):
     imgs = np.array([img])
     res = model.predict(imgs)
     index = np.argmax(res)
     return str(index)
 
-# Phần code của OpenCV.
+# Hàm xác định cửa sổ camera đã được click chưa.
 startInference = False
 
 # Nếu click vào màn hình thì ứng dụng sẽ bắt đầu lấy ảnh.
@@ -22,7 +22,7 @@ def ifClicked(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         startInference = not startInference
 
-# threshold slider handler 
+# Kiểm soát giá trị ngưỡng.
 threshold = 100
 def on_threshold(x):
     global threshold
@@ -43,47 +43,48 @@ def start_cv(model):
 
         if (startInference):
             
-            # frame counter for showing text 
+            # Đếm số khung hình hiện tại.
             frameCount += 1
 
-            # black outer frame
+            # Làm một khung màu đen để giới hạn khung hình.
             frame[0:480, 0:80] = 0
             frame[0:480, 560:640] = 0
             grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # apply threshold
+            # Áp dụng giá trị ngưỡng.
             _, thr = cv2.threshold(grayFrame, threshold, 255, cv2.THRESH_BINARY_INV)
            
 
-            # get central image 
-            resizedFrame = thr[120:360, 200:440]  # Adjusted the region of interest
-            background[120:360, 200:440] = resizedFrame  # Adjusted the region of interest
+            # Trích xuất ảnh từ khung hình trung tâm.
+            resizedFrame = thr[120:360, 200:440]
+            background[120:360, 200:440] = resizedFrame
 
-            # resize for inference 
+            # Thay đổi kích thước cho phù hợp mô hình.
             iconImg = cv2.resize(resizedFrame, (28, 28))
             
-            # pass to model predictor 
+            # Dự đoán khung hình vừa lấy.
             res = predict(model, iconImg)
 
-            # clear background 
+            # Mỗi 5 khung hình thì số ở góc sẽ được reset. 
             if frameCount == 5:
                 background[0:480, 0:80] = 0
                 frameCount = 0
 
-            # show text 
+            # In ra dự đoán ở góc màn hình.
             cv2.putText(background, res, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
             cv2.rectangle(background, (200, 120), (440, 360), (255, 255, 255), thickness=3)  # Adjusted the region of interest
             
-            # display frame 
+            # Áp dụng khung giới hạn màu đen.
             cv2.imshow('background', background)
         else:
-            # display normal video 
+            # Chạy cam bình thường.
             cv2.imshow('background', frame)
 
-        # cv2.imshow('resized', resizedFrame)
+        # Nếu phím 'q' được bấm, dừng ứng dụng.
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
-
+    
+    # Tắt camera và đóng cửa sổ.
     cap.release()
     cv2.destroyAllWindows()
 
@@ -91,7 +92,7 @@ def start_cv(model):
 
 
 def run():
-    # Load the model with custom objects
+    # Gọi model với hàm softmax.
     model = tf.keras.models.load_model('model.h5', custom_objects={'softmax_v2': softmax_v2})
     print('Loaded saved model.')
     print(model.summary())  

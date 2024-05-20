dadca6b77686a5eb72aa1771229a633fdd1d0a12
MinHius <125289254+MinHius@users.noreply.github.com>
2024-05-20 22:40:52 +0700

    fix

--- main.py
+++ main.py
@@ -24,53 +24,64 @@ def ifClicked(event, x, y, flags, params):
 
 # Mở webcam và capture ảnh.
 def start_cv(model):
-    global startInference
+    global threshold
     cap = cv2.VideoCapture(0)
     frame = cv2.namedWindow('background')
     cv2.setMouseCallback('background', ifClicked)
-
+    cv2.createTrackbar('threshold', 'background', 150, 255, on_threshold)
     background = np.zeros((480, 640), np.uint8)
     frameCount = 0
-    
-    # Threshold.
-    threshold = 150  
 
     while True:
         ret, frame = cap.read()
 
-        if startInference:
+        if (startInference):
+            
+            # frame counter for showing text 
             frameCount += 1
 
+            # black outer frame
             frame[0:480, 0:80] = 0
             frame[0:480, 560:640] = 0
             grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+            # apply threshold
             _, thr = cv2.threshold(grayFrame, threshold, 255, cv2.THRESH_BINARY_INV)
+           
 
+            # get central image 
             resizedFrame = thr[240-75:240+75, 320-75:320+75]
             background[240-75:240+75, 320-75:320+75] = resizedFrame
 
+            # resize for inference 
             iconImg = cv2.resize(resizedFrame, (28, 28))
             
-            # Dự đoán ảnh vừa thu được.
+            # pass to model predictor 
             res = predict(model, iconImg)
 
+            # clear background 
             if frameCount == 5:
                 background[0:480, 0:80] = 0
                 frameCount = 0
 
+            # show text 
             cv2.putText(background, res, (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 3)
             cv2.rectangle(background, (320-80, 240-80), (320+80, 240+80), (255, 255, 255), thickness=3)
             
+            # display frame 
             cv2.imshow('background', background)
         else:
+            # display normal video 
             cv2.imshow('background', frame)
 
+        # cv2.imshow('resized', resizedFrame)
         if cv2.waitKey(1) & 0xff == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+
+
 
 def run():
     # Load the model with custom objects
